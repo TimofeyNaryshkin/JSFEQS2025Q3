1e5b598d667b546339da9c20ce0b4e5f5e1e95a9
Timofey <naryshkint4@gmail.com>
2025-10-26 19:06:52 +0900

    feat: add cart item styles

--- coffee-house/src/components/CartItem.ts
+++ coffee-house/src/components/CartItem.ts
@@ -3,22 +3,36 @@ import createElement from "../utils/create-element";
 
 export const CartItem = (item: TCartItem) => {
   const cartItem = createElement("div", "cart__item");
-  const removeButton = createElement("button", "button button_cart-item__remove action");
+  const cartItemInfo = createElement("div", "cart__item__info");
+  const removeButton = createElement(
+    "button",
+    "button button_cart-item_remove action"
+  );
   removeButton.setAttribute("data-product-id", item.id.toString());
-  const itemImage = createElement("div", "cart__item__image");
+  const itemImage = createElement("div", `cart__item__image ${item.categoty}_${item.id}`);
   const itemDescription = createElement("div", "cart__item__description");
   const itemTitle = createElement("h3", undefined, item.name);
-  const itemOptions = createElement("p", "medium", `${[item.size, ...item.extras].join(',')}`);
-  const itemPriceContainer = createElement("div", "cart__item__price-container");
-  const itemPrice = createElement("p", "cart__item__price", `${item.size}`);
-  const itemDiscountPrice = createElement("p", "cart__item__discount-price", `${item.discountPrice}`);
+  const itemOptions = createElement(
+    "p",
+    "medium",
+    `${[item.size, ...item.extras].join(",")}`
+  );
+  const itemPriceContainer = createElement(
+    "div",
+    "cart__item__price-container"
+  );
+  const itemPrice = createElement("h3", "cart__item__price", `$${item.prise}`);
+  const itemDiscountPrice = createElement(
+    "h3",
+    "cart__item__discount-price",
+    `$${item.discountPrice}`
+  );
 
   itemDescription.append(itemTitle, itemOptions);
   itemPriceContainer.append(itemPrice, itemDiscountPrice);
-  cartItem.append(removeButton, itemImage, itemDescription, itemPriceContainer);
+  cartItemInfo.append(removeButton, itemImage, itemDescription);
+  cartItem.append(cartItemInfo, itemPriceContainer);
 
-  removeButton.addEventListener("click", () => {
-
-  })
+  removeButton.addEventListener("click", () => {});
   return cartItem;
 };
