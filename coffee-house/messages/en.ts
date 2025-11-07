5953385e4c2a04b2fa5e9d255e1ab468f85eacf3
Timofey Naryshkin <naryshkint4@gmail.com>
2025-11-07 20:34:55 +0900

    feat: set up next-intl

---
+++ coffee-house/messages/en.ts
@@ -0,0 +1,77 @@
+export const en = {
+  basic: {
+    confirm: "Confirm",
+    signIn: "Sign In",
+    registration: "Registration",
+    signOut: "Sign Out",
+    main: "Main",
+    unexpectedError: "An unexpected error occurred.",
+    yes: "Yes",
+    no: "No",
+    menu: "Menu",
+    language: "Language",
+  },
+  nav: {
+    favCoffee: "Favorite coffee",
+    about: "About",
+    mobileApp: "Mobile app",
+    cta: "Contact us",
+  },
+  mainPage: {
+    enjoy: "Enjoy premium coffee at our charming cafe",
+    enjoyText:
+      "With its inviting atmosphere and delicious coffee options, the Coffee House Resource is a popular destination for coffee lovers and those seeking a warm and inviting space to enjoy their favorite beverage. ",
+    chooseCoffeeHeading: "Choose your favorite coffee",
+    aboutHeading:
+      "Resource is the perfect and cozy place where you can enjoy a variety of hot beverages, relax, catch up with friends, or get some work done. ",
+    mobileAppHeading: "Download our apps to start ordering",
+    mobileAppText:
+      "Download the Resource app today and experience the comfort of ordering your favorite coffee from wherever you are ",
+    footerHeading: "Sip, Savor, Smile. It’s coffee time!",
+  },
+  menuPage: {
+    menuHeading: "Behind each of our cups hides an amazing surprise",
+    coffee: "Coffee",
+    tea: "Tea",
+    dessert: "Dessert",
+    addToCart: "Add to cart",
+  },
+  cartPage: {
+    cartHeading: "Cart",
+  },
+  messages: {
+    thxForOrder:
+      "Thank you for your order! Our manager will contact you shortly.",
+    smthWentWrong: "Something went wrong. Please, refresh the page",
+  },
+  auth: {
+    login: "Login",
+    password: "Password",
+    confirmPassword: "Confirm password",
+    city: "City",
+    street: "Street",
+    houseNumber: "House number",
+    payBy: "Pay by",
+    cash: "Cash",
+    card: "Card",
+  },
+  errorPage: {
+    text: "Page Not Found",
+    link: "Go to Main",
+  },
+  validations: {
+    required: "Required field",
+    nameCapitalized: "Name must start with a capital letter",
+    emailInvalid: "Invalid email address",
+    passwordMinLength: "Password must be at least 8 characters long",
+    passwordStrength: "Must include: 0-9, A-Z, a-z & special character",
+    passwordsMustMatch: "Passwords must match",
+  },
+  errors: {
+    readError: "Failed to load data",
+    saveError: "Failed to save data",
+    deleteError: "Failed to delete data",
+  },
+};
+
+export type LanguagesObjType = typeof en;
---
+++ coffee-house/src/app/[locale]/layout.tsx
@@ -0,0 +1,29 @@
+import Header from '@/components/layout/Header/Header';
+import { routing } from '@/i18n/routing';
+import { NextIntlClientProvider, hasLocale } from 'next-intl';
+import { getMessages } from 'next-intl/server';
+import { notFound } from 'next/navigation';
+
+type Props = {
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+};
+
+export default async function LocaleLayout({ children, params }: Props) {
+  // Ensure that the incoming `locale` is valid
+  const { locale } = await params;
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
+  const messages = await getMessages({ locale })
+
+  return (
+    <NextIntlClientProvider locale={locale} messages={messages}>
+      <Header />
+      <main>
+        {children}
+      </main>
+    </NextIntlClientProvider>
+  )
+}
