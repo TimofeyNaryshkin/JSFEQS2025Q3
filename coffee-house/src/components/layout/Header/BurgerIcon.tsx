8d8dbecd5bdedca95a19d1ae1bcacf30ba058bea
Timofey Naryshkin <naryshkint4@gmail.com>
2025-11-07 21:35:18 +0900

    feat: install lucide-react, add cart, menu and burger icons, implement header

---
+++ coffee-house/src/components/layout/Header/BurgerIcon.tsx
@@ -0,0 +1,9 @@
+import { Menu } from "lucide-react";
+
+export default function BurgerIcon() {
+  return (
+    <button className="w-11 h-11 flex items-center justify-center border-solid">
+      <Menu width={20} height={20} />
+    </button>
+  )
+}
--- coffee-house/src/components/layout/Header/Header.tsx
+++ coffee-house/src/components/layout/Header/Header.tsx
@@ -1,11 +1,19 @@
+import Link from "next/link";
 import Logo from "./Logo";
 import Nav from "./Nav";
+import { Coffee, Menu, ShoppingBag } from "lucide-react";
+import BurgerIcon from "./BurgerIcon";
 
 export default function Header() {
   return (
-    <header>
+    <header className="flex justify-between items-center">
       <Logo />
       <Nav />
+      <div className="flex gap-8 items-center">
+        <Link href='/cart' className="flex gap-2"><ShoppingBag width={20} height={20} /></Link>
+        <Link href='/menu' className="flex gap-2">Menu<Coffee width={20} height={20} /></Link>
+        <BurgerIcon />
+      </div>
     </header>
   )
 }
