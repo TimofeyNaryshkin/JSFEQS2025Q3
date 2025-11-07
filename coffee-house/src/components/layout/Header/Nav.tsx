ba7035187a536f9056ad69ca2b6d5a7a39b1f6b0
Timofey Naryshkin <naryshkint4@gmail.com>
2025-11-07 22:12:10 +0900

    feat: add nav link and header buttons styles

--- coffee-house/src/components/layout/Header/Nav.tsx
+++ coffee-house/src/components/layout/Header/Nav.tsx
@@ -1,26 +1,26 @@
 import translate from "@/i18n/langSwitcher";
-import { useLocale, useTranslations } from "next-intl";
+import { getLocale } from "next-intl/server";
 import Link from "next/link";
 
-export default function Nav() {
+export default async function Nav() {
 
-  const locale = useLocale()
+  const locale = await getLocale()
   const { nav: t } = translate(locale)
 
   return (
     <nav>
       <ul className="flex gap-10">
         <li>
-          <Link href='/#favorite'>{t.favCoffee}</Link>
+          <Link href='/#favorite' className="relative transition-colors after:absolute after:top-5 after:left-0 after:h-0.5 after:w-0 after:bg-current after:transition-all after:duration-300 hover:after:w-full">{t.favCoffee}</Link>
         </li>
         <li>
-          <Link href='/#about'>{t.about}</Link>
+          <Link href='/#about' className="relative transition-colors after:absolute after:top-5 after:left-0 after:h-0.5 after:w-0 after:bg-current after:transition-all after:duration-300 hover:after:w-full">{t.about}</Link>
         </li>
         <li>
-          <Link href='/#mobile-app'>{t.mobileApp}</Link>
+          <Link href='/#mobile-app' className="relative transition-colors after:absolute after:top-5 after:left-0 after:h-0.5 after:w-0 after:bg-current after:transition-all after:duration-300 hover:after:w-full">{t.mobileApp}</Link>
         </li>
         <li>
-          <Link href='#contact'>{t.cta}</Link>
+          <Link href='#contact' className="relative transition-colors after:absolute after:top-5 after:left-0 after:h-0.5 after:w-0 after:bg-current after:transition-all after:duration-300 hover:after:w-full">{t.cta}</Link>
         </li>
       </ul>
     </nav>
