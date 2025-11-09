3edce61a4ad7b580105dcfbff8179c46378151b6
Timofey Naryshkin <naryshkint4@gmail.com>
2025-11-09 20:01:19 +0900

    feat: implement items quantity display in cart icon

--- coffee-house/src/components/layout/Header/Header.tsx
+++ coffee-house/src/components/layout/Header/Header.tsx
@@ -5,12 +5,13 @@ import { Coffee, ShoppingBag } from "lucide-react";
 import BurgerIcon from "./BurgerIcon";
 import { getLocale } from "next-intl/server";
 import translate from "@/i18n/langSwitcher";
+import CartIcon from "./CartIcon";
 
 export default async function Header() {
   const locale = await getLocale()
   const { mainPage, nav } = translate(locale)
 
-  const linkStyle = "flex gap-2 relative transition-colors after:absolute after:top-5.5 after:left-0 after:h-0.5 after:w-0 after:bg-current after:transition-all after:duration-300 hover:after:w-full"
+  const linkStyle = "flex items-center gap-2 relative transition-colors after:absolute after:top-[24.5px] after:left-0 after:h-0.5 after:w-0 after:bg-current after:transition-all after:duration-300 hover:after:w-full"
 
   return (
     <header className="my-5">
@@ -18,7 +19,7 @@ export default async function Header() {
         <Logo />
         <Nav localeText={nav} />
         <div className="flex gap-8 items-center">
-          <Link href='/cart' className={linkStyle}><ShoppingBag width={20} height={20} /></Link>
+          <CartIcon className={linkStyle} />
           <Link href='/menu' className={linkStyle}>{mainPage.menu}<Coffee width={20} height={20} /></Link>
           <BurgerIcon />
         </div>
